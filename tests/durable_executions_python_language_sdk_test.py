6d9f0ad4cd40197fa887aa5cf6f35baf29bf5290
hsilan <hsilan@amazon.com>
2025-10-01 09:16:22 -0700

    first commit

--- tests/durable_executions_python_language_sdk_test.py
+++
@@ -1,6 +0,0 @@
-"""Tests for DurableExecutionsPythonLanguageSDK module."""
-
-
-def test_aws_durable_execution_sdk_python_importable():
-    """Test aws_durable_execution_sdk_python is importable."""
-    import aws_durable_execution_sdk_python  # noqa: F401
